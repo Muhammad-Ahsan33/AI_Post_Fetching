b91bf346938370f798818ebc2e39964e57a5caff
Ahsan <157168564+Muhammad-Ahsan33@users.noreply.github.com>
2026-02-04 23:01:31 +0500

    Uncomment and add keywords for hiring artists

--- app/keywords.py
+++ app/keywords.py
@@ -11,50 +11,39 @@ KEYWORDS = [
     "seeking artist",
     "seeking animator",
     "someone to commission",
-    "someone to comm"
+    "someone to comm",
+    # Research / discovery with intent
+    "anyone know an artist",
+    "anyone know any artist",
+    "need to find artist",
+    "trying to find artist",
+    "tryna find artist",
+    "who to commission",
+    "who to comm",
+
+    # Conditional / future intent
+    "might commission",
+    "might comm",
+    "thinking of commissioning",
+    "going to commission",
+    "willing to commission",
+
+    # Direct requests
+    "can someone draw",
+    "need drawn",
+    "want drawn",
+
+    # Hiring for specific work (buyer + verb)
+    "looking for animator",
+    "need animator",
+    "need rigger",
+    "need rigging done",
+    "need animation done",
+    "need model",
+    "need vtuber",
+    "need thumbnail",
+    "need emotes",
+    "need logo",
+    "need banner",
+    "need chibi"
 ]
-
-#     # Research / discovery with intent
-#     "anyone know an artist",
-#     "anyone know any artist",
-#     "need to find artist",
-#     "trying to find artist",
-#     "tryna find artist",
-#     "who to commission",
-#     "who to comm",
-
-#     # Conditional / future intent
-#     "might commission",
-#     "might comm",
-#     "thinking of commissioning",
-#     "going to commission",
-#     "willing to commission",
-
-#     # Direct requests
-#     "can someone draw",
-#     "need drawn",
-#     "want drawn",
-
-#     # Hiring for specific work (buyer + verb)
-#     "looking for animator",
-#     "need animator",
-#     "need rigger",
-#     "need rigging done",
-#     "need animation done",
-#     "need model",
-#     "need vtuber",
-#     "need thumbnail",
-#     "need emotes",
-#     "need logo",
-#     "need banner",
-#     "need chibi",
-# ]
-
-
-
-
-# KEYWORDS = [
-#     # STRONG buyer intent (explicit)
-#     "looking for artist",
-#     "looking to commission",
-# ]
